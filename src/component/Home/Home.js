436e022e336f1110cf48f8415f898f14c10a3114
parvez13913 <parvezz13913@gmail.com>
2022-04-03 21:24:48 +0600

    Logo and title add

---
+++ src/component/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <div>
+                <h1>The Miracle Morning</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
