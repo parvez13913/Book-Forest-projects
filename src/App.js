8362af6d6a27c1c3e5d5350568f89f5ec6d5c4a3
parvez13913 <parvezz13913@gmail.com>
2022-04-04 00:30:28 +0600

    header complite

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Header from './component/Header/Header'
+import Header from './component/Header/Header';
 import Home from './component/Home/Home';
+import Review from './component/Reviews/Reviews';
+import Dashboard from './component/Dashboard/Dashboard';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Routes>
-        <Route path='/' element= {<Home></Home>}></Route>
-        <Route path='/home' element= {<Home></Home>}></Route>
+        <Route path='/' element = {<Home></Home>}></Route>
+        <Route path='/home' element = {<Home></Home>}></Route>
+        <Route path='/review' element = {<Review></Review>}></Route>
+        <Route path='/dashboard' element = {<Dashboard></Dashboard>}></Route>
       </Routes>
     </div>
   );
--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className='bg-green-300 p-4 flex justify-around'>
+            <div>
+                <Link to = '/home' className='text-gray-500 font-bold text-lg md:text-3xl font-serif'>BooK Forest</Link>
+            </div>
+            <nav className='navigation font-thin md: text-lg font-semibold'>
+                <Link to = '/home'>Home</Link>
+                <Link to = '/review'>Reviews</Link>
+                <Link to = '/dashboard'>Dashboard</Link>
+            </nav>
         </div>
     );
 };
