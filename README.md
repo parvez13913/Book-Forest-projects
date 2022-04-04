ff4493537c59d5e6b000b9e34fbc2c37e1dab19c
parvez13913 <parvezz13913@gmail.com>
2022-04-04 21:46:53 +0600

    Readme file update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Getting Started with Create React App
+# Getting Started with Book Forest
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project was bootstrapped with [Book Forest](https://leafy-parfait-f6d9fd.netlify.app/review).
 
 ## Available Scripts
 
--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 
 const Footer = () => {
     return (
-        <div className='text-center bg-slate-900 p-12 mb-3 mt-8 md:mt-3 lg:mt-2'>
+        <div className='text-center bg-slate-900 p-12 mb-3 mt-8 grid grid-cols-1 md:mt-3 lg:mt-2'>
             <h1 className='text-4xl text-slate-300 font-serif'>Foolish Developer</h1>
             <div className='footer flex justify-center'>
                   <img src="images/Developer-photo.jpg" alt="developerPhoto" />
